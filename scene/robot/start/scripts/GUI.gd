002b435f73c320dc8617a72e01083a4c8026a8b0
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2025-06-03 07:30:05 +0200

    Suppression du dossier scene

--- scene/robot/start/scripts/GUI.gd
+++
@@ -1,8 +0,0 @@
-extends MarginContainer
-
-onready var number_label = $Bars/LifeBar/Count/Background/Number
-onready var bar = $Bars/LifeBar/TextureProgress
-onready var tween = $Tween
-
-func _ready():
-	pass
